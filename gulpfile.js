eb89bc1b2de633b9c6823d6f4c371e34cb574e5b
Mat Sz <contact@matsz.dev>
2020-06-12 18:45:57 +0200

    added the charcoal font

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const path = require('path');
+
+const postcss = require('gulp-postcss');
+const postcssUrl = require('postcss-url');
 
 gulp.task('sass', () => {
+  const plugins = [
+    postcssUrl({
+      url: 'inline',
+      basePath: path.resolve('./src/'),
+      assetsPath: path.resolve('./demo/assets/'),
+      useHash: true,
+    }),
+  ];
+
   return gulp
     .src('src/*.scss')
     .pipe(sass())
+    .pipe(postcss(plugins), { syntax: require('postcss-scss') })
     .pipe(gulp.dest('demo/css'))
     .pipe(browserSync.stream());
 });
